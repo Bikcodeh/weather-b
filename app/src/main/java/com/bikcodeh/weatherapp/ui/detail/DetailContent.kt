8d7779b1e0bc465ad42e62f1aad75226ca0e4398
Victor Hoyos <bikcodeh@gmail.com>
2025-12-15 13:12:07 -0500

    fix: add scroll to detail view

--- app/src/main/java/com/bikcodeh/weatherapp/ui/detail/DetailContent.kt
+++ app/src/main/java/com/bikcodeh/weatherapp/ui/detail/DetailContent.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,7 +41,7 @@ fun DetailContent(state: DetailState) {
     } else {
         if (currentForecast == null || currentWeather == null) return
 
-        Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
             WeatherDetailHeader(
                 condition = currentForecast.condition!!,
                 temperature = currentWeather.temperatureCelsius!!,
